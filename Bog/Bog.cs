f3e5e1b6a3783c8cf99ea8847ec9337f19ba39f8
ProKrillz <thomasdamkjaer@gmail.com>
2022-06-01 19:37:08 +0200

    Opgave 1

---
+++ Bog/Bog.cs
@@ -0,0 +1,30 @@
+﻿namespace Bog
+{
+    internal class Bogen
+    {
+        int _bogId = default!;
+        string _titel = default!;
+        string _forfatter = default!;
+        string _isbnNummer = default!;
+        DateTime _udgivelse = default!;
+
+        public Bogen(int bogId, string titel, string forfatter, string isbnNummer, DateTime udgivelse)
+        {
+            _bogId = bogId;
+            _titel = titel;
+            _forfatter = forfatter;
+            _isbnNummer = isbnNummer;
+            _udgivelse = udgivelse;
+        }
+        public string HentBog()
+        {
+            return $"Bog Id: {_bogId}\nTitel: {_titel}\nForfatter: {_forfatter}\nIsbn nummer: {_isbnNummer}\nUdgivelse: {_udgivelse.Year}";
+        }
+        public string HentBogTitel()
+        {
+            return $"Titel: {_titel}";
+        }
+    }
+   
+
+}
--- Bog/Program.cs
+++ Bog/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bog;
+
+class Program
+{
+    public static void Main()
+    {
+        
+        Bogen DeBlå = new Bogen(1, "De blå bæster", "R.L Stine", "8721018707", new DateTime(2002, 01, 01));
+        Bogen BogenOmC = new Bogen(2, "Bogen om C# 9.0", "Michell Cronberg", "9788799338238", new DateTime(2021, 01, 01));
+        Console.WriteLine(DeBlå.HentBog());
+        Console.WriteLine(DeBlå.HentBogTitel());
+        Console.WriteLine(BogenOmC.HentBog());
+    }
+}
