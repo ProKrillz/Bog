90d77bd2ecdaf6555a022c8e040484741e0d67e1
ProKrillz <thomasdamkjaer@gmail.com>
2022-06-02 13:40:33 +0200

    ...

--- Bog/Bog.cs
+++ Bog/Bog.cs
@@ -7,32 +7,12 @@
         public string Forfatter { get; set; }
         public string IsbnNummer { get; set; }
         public DateTime Udgivelse { get; set; }
-
-
-        int _bogId = default!;
-        string _titel = default!;
-
         public Bogen() { }
 
         public Bogen(int bogId, string titel)
         {
-<<<<<<< HEAD
-            _bogId = bogId;
-            _titel = titel;
-            _forfatter = forfatter;
-            _isbnNummer = isbnNummer;
-            _udgivelse = udgivelse;
-        }
-        public Bogen(string titel, string forfatter, string isbnNummer, DateTime udgivelse)
-        {
+            BogId = bogId;
             Titel = titel;
-            Forfatter = forfatter;
-            IsbnNummer = isbnNummer;
-            Udgivelse = udgivelse;
-        }
-        public string HentBog()
-        {
-            return $"Bog Id: {_bogId}\nTitel: {_titel}\nForfatter: {_forfatter}\nIsbn nummer: {_isbnNummer}\nUdgivelse: {_udgivelse.Year}";
         }
     }
 }
