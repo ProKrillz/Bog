007817375b9482209d599ecce77e9174766d193a
ProKrillz <thomasdamkjaer@gmail.com>
2022-06-02 13:30:17 +0200

    opgave 3

---
+++ Bog/Blad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bog
+{
+    public class Blad
+    {
+        public int BladId { get; set; }
+        public string Titel { get; set; }
+        public string IsbnNummer { get; set; }
+        public DateTime Udgivelse { get; set; }
+        public Blad() { }
+        public Blad(int bladId, string titel, string isbnNummer, DateTime udgivelse)
+        {
+            BladId = bladId;
+            Titel = titel;
+            IsbnNummer = isbnNummer;
+            Udgivelse = udgivelse;
+        }
+    }
+}
--- Bog/Bog.cs
+++ Bog/Bog.cs
@@ -2,29 +2,18 @@
 {
     internal class Bogen
     {
-        int _bogId = default!;
-        string _titel = default!;
-        string _forfatter = default!;
-        string _isbnNummer = default!;
-        DateTime _udgivelse = default!;
+        public int BogId { get; set; }
+        public string Titel { get; set; }
+        public string Forfatter { get; set; }
+        public string IsbnNummer { get; set; }
+        public DateTime Udgivelse { get; set; }
 
-        public Bogen(int bogId, string titel, string forfatter, string isbnNummer, DateTime udgivelse)
+        public Bogen() { }
+
+        public Bogen(int bogId, string titel)
         {
-            _bogId = bogId;
-            _titel = titel;
-            _forfatter = forfatter;
-            _isbnNummer = isbnNummer;
-            _udgivelse = udgivelse;
-        }
-        public string HentBog()
-        {
-            return $"Bog Id: {_bogId}\nTitel: {_titel}\nForfatter: {_forfatter}\nIsbn nummer: {_isbnNummer}\nUdgivelse: {_udgivelse.Year}";
-        }
-        public string HentBogTitel()
-        {
-            return $"Titel: {_titel}";
+            BogId = bogId;
+            Titel = titel;
         }
     }
-   
-
 }
